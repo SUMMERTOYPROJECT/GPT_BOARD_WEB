f183d5a8c381e4cc4905c0e4a1583e70df9e6a54
Junyong <jypark93@kookmin.ac.kr>
2024-01-31 02:22:45 +0900

    #7-db 의존성 업데이트 및 JPA 테스트 확인
    MySQL연동 후 엔티티간 연동 테스트 완료
    * TODO :  Password 설정을 숨겨야함

---
+++ src/main/java/com/jyp_board/board_app/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package com.jyp_board.board_app.config;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> audotprAware(){
+        return () -> Optional.of("jyp");
+    }
+}
